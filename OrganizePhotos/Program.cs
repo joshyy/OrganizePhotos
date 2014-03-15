f6d6c54af840fec20e5bc8b4a53afa9720cc9899
Joshua B <tornworm@yahoo.com>
2014-03-15 08:25:45 -0400

    Search directories recursively and keep duplicates
    Search directories recursively and keep duplicates with a sequence
    number.

--- OrganizePhotos/Program.cs
+++ OrganizePhotos/Program.cs
@@ -6,21 +6,59 @@ namespace OrganizePhotos
 {
     class Program
     {
+        //todo: delete old (empty) directories
         static void Main(string[] args)
         {
             string folder = args.Length == 0 ? "." : args[0];
+            string ext = "*.jpg";
 
-            foreach (string file in Directory.EnumerateFiles(folder, "*.jpg"))
+            // create a new directory to hold all of the organized photos, this directory will be ignored when the program is run multiple times
+            // this is the directory where the photos will be stored
+            string creDirectory = folder + Path.DirectorySeparatorChar + "OrganizePhotos";
+            Directory.CreateDirectory(creDirectory);
+            Console.WriteLine("Cre dir: " + creDirectory);
+
+            foreach (string dir in Directory.EnumerateDirectories(folder))
             {
-                DateTime dateTime;
-                using (var reader = new ExifReader(file))
-                {
-                    if (!reader.GetTagValue(ExifTags.DateTimeOriginal, out dateTime)) continue;
-                }
+                // skip the creDirectory
+                if (dir == creDirectory) { continue; }
 
-                string subFolder = Path.Combine(folder, dateTime.ToString("yyyy-MM-dd"));
-                Directory.CreateDirectory(subFolder);
-                File.Move(file, Path.Combine(subFolder, Path.GetFileName(file)));
+                foreach (string file in Directory.EnumerateFiles(dir, ext, SearchOption.AllDirectories))
+                {
+                    int dupSeq = 0;
+                    DateTime dateTime;
+                    using (var reader = new ExifReader(file))
+                    {
+                        if (!reader.GetTagValue(ExifTags.DateTimeOriginal, out dateTime)) continue;
+                    }
+
+                    string subFolder = Path.Combine(creDirectory, dateTime.ToString("yyyy-MM-dd"));
+                    Directory.CreateDirectory(subFolder);
+                    string newFileName = Path.Combine(subFolder, Path.GetFileName(file));
+
+                    if (File.Exists(newFileName))
+                    {
+                        // a sequential number is appended to each duplicate file name (may not be the same photo)
+                        string seqFileName = string.Format("{0}_{1}{2}", Path.GetFileNameWithoutExtension(newFileName), ++dupSeq, Path.GetExtension(newFileName));
+                        newFileName = Path.Combine(subFolder, Path.GetFileName(seqFileName));
+
+                        while (File.Exists(newFileName))
+                        {
+                            seqFileName = string.Format("{0}_{1}{2}", Path.GetFileNameWithoutExtension(file), ++dupSeq, Path.GetExtension(newFileName));
+                            Console.WriteLine("duplicate file {0} - renaming {1}", newFileName, seqFileName);
+                            newFileName = Path.Combine(subFolder, Path.GetFileName(seqFileName));
+                        }
+                    }
+                    try
+                    {
+                        File.Move(file, newFileName);
+                    }
+                    catch (Exception m)
+                    {
+                        Console.WriteLine(m.Message);
+                        Console.WriteLine(newFileName);
+                    }
+                }
             }
         }
     }
